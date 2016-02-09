e40f61fbac3088cd771561c048a923a3dd6b16b0
Samuel Roberts <sproberts92@outlook.com>
2016-02-09 16:22:38 +0100

    Factor out setUp and tearDown of db into a function
    Takes db class as an argument so can construct either Database
    or Dictionary depending on which is passed.

--- test/test_dictsr.py
+++ test/test_dictsr.py
@@ -28,20 +28,26 @@ class WordTestCase(unittest.TestCase):
         with self.assertRaises(TypeError):
             type_test = dictsr.Word("Word", "Function", "Def1")
 
-class DatabaseTestCase(unittest.TestCase):
-
-    def setUp(self):
+def setUpDb(self, db):
         self.path = "test/databases/test_db"
         self.dirname = os.path.dirname(self.path)
-        self.database = dictsr.Database(self.path)
+        self.test_db = db(self.path)
 
-    def tearDown(self):
-        del self.database
+def tearDownDb(self):
+        del self.test_db
         if os.path.exists(self.path):
             os.remove(self.path)
         if os.path.exists(self.dirname):
             os.rmdir(self.dirname)
 
+class DatabaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        setUpDb(self, dictsr.Database)
+        
+    def tearDown(self):
+        tearDownDb(self)
+        
     def test_dir_init(self):
         self.assertTrue(os.path.isfile(self.path))
 
@@ -50,25 +56,19 @@ class DatabaseTestCase(unittest.TestCase):
             database = dictsr.Database(1)
 
     def test_connection_type(self):
-        self.assertIs(type(self.database.conn), sqlite3.Connection)
+        self.assertIs(type(self.test_db.conn), sqlite3.Connection)
 
     def test_cursor_type(self):
-        self.assertIs(type(self.database.c), sqlite3.Cursor)
+        self.assertIs(type(self.test_db.c), sqlite3.Cursor)
 
 class DictionarySetUpTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.path = "test/databases/test_db"
-        self.dirname = os.path.dirname(self.path)
-        self.test_dict = dictsr.Dictionary(self.path)
-
+        setUpDb(self, dictsr.Dictionary)
+        
     def tearDown(self):
-        del self.test_dict
-        if os.path.exists(self.path):
-            os.remove(self.path)
-        if os.path.exists(self.dirname):
-            os.rmdir(self.dirname)
-
+        tearDownDb(self)
+        
     def test_dir_init(self):
         self.assertTrue(os.path.isfile(self.path))
 
@@ -77,68 +77,63 @@ class DictionarySetUpTestCase(unittest.TestCase):
             test_dict = dictsr.Dictionary(1)
 
     def test_connection_type(self):
-        self.assertIs(type(self.test_dict.conn), sqlite3.Connection)
+        self.assertIs(type(self.test_db.conn), sqlite3.Connection)
 
     def test_cursor_type(self):
-        self.assertIs(type(self.test_dict.c), sqlite3.Cursor)
+        self.assertIs(type(self.test_db.c), sqlite3.Cursor)
 
     def test_table_creation(self):
         compare = [('table', 'dictionary', 'dictionary', 2)]
 
-        self.test_dict.c.execute('''
+        self.test_db.c.execute('''
             SELECT type, name, tbl_name, rootpage
             FROM sqlite_master
         ''')
 
-        self.assertEqual(self.test_dict.c.fetchall(), compare)
+        self.assertEqual(self.test_db.c.fetchall(), compare)
 
     def test_create_when_table_already_exists(self):
         test_db = dictsr.Dictionary(self.path)
 
+
 class DictionaryAddWordTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.path = "test/databases/test_db"
-        self.dirname = os.path.dirname(self.path)
-        self.test_dict = dictsr.Dictionary(self.path)
+        setUpDb(self, dictsr.Dictionary)
         self.test_word = dictsr.Word("Word", [("Fn1", "Def1"), ("Fn2", "Def2")])
 
     def tearDown(self):
-        del self.test_dict
-        if os.path.exists(self.path):
-            os.remove(self.path)
-        if os.path.exists(self.dirname):
-            os.rmdir(self.dirname)
+        tearDownDb(self)
 
     def test_as_SQL_tuples(self):
-        result = self.test_dict.as_SQL_tuples(self.test_word)
+        result = self.test_db.as_SQL_tuples(self.test_word)
         self.assertEqual(result, [("Word", "Fn1", "Def1"),("Word", "Fn2", "Def2")])
 
     def test_add_word(self):
-        self.test_dict.add_word(self.test_word)
+        self.test_db.add_word(self.test_word)
 
-        self.test_dict.c.execute('''
+        self.test_db.c.execute('''
             SELECT *
             FROM dictionary
         ''')
 
         self.assertEqual(
-            self.test_dict.c.fetchall(),
-            self.test_dict.as_SQL_tuples(self.test_word)
+            self.test_db.c.fetchall(),
+            self.test_db.as_SQL_tuples(self.test_word)
         )
 
     def test_uniqueness_of_entries(self):
-        self.test_dict.add_word(self.test_word)
-        self.test_dict.add_word(self.test_word)
+        self.test_db.add_word(self.test_word)
+        self.test_db.add_word(self.test_word)
 
-        self.test_dict.c.execute('''
+        self.test_db.c.execute('''
             SELECT *
             FROM dictionary
         ''')
 
         self.assertEqual(
-            self.test_dict.c.fetchall(),
-            self.test_dict.as_SQL_tuples(self.test_word)
+            self.test_db.c.fetchall(),
+            self.test_db.as_SQL_tuples(self.test_word)
         )
 
 if __name__ == '__main__':
